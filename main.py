3a177bff91b3d0c7e72b644bc3bb6a1a5ecd4787
lazaromasot <lazz84@hotmail.com>
2023-04-15 13:59:33 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Millennials Band Name Generator")
+city = input("In what city did you grew in?\n ")
+pet = input("What is the name of your pet?\n ")
+print(f"Your Band Name could be {city} {pet}")
